b55894a1b1b2226de377b252d72da2d5dfb69a84
Anuj Mishra <anujmishraan2005@gmail.com>
2024-03-10 21:51:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
-
 let int = setInterval(blurring, 30)
 
 function blurring() {
@@ -11,7 +10,6 @@ function blurring() {
   if (load > 99) {
     clearInterval(int)
   }
-
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
